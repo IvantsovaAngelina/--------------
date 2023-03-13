8010449a4c182a4ef330d5e686e1c05c24cb4956
Ivantsova Angelina <iva.angelina.a@gmail.com>
2023-03-13 17:38:48 +0500

    modified:   diplom/src/page/GallaryActive/GallaryActive.js

--- diplom/src/page/GallaryActive/GallaryActive.js
+++ diplom/src/page/GallaryActive/GallaryActive.js
@@ -1,8 +1,20 @@
+import UnitGallary from '../../components/unitGallary/UnitGallary';
 import './style-gallaryActive.css'
 
 const GallaryActive = () => {
     return ( 
-        <h1>Кек</h1>
+        <>
+            <div className='container'>
+                <div className='row justify-content-center'>
+                    <div className='col-md-12'>
+                        <h1>Галерея работ</h1>
+                    </div>
+                </div>
+                <div className='row'>
+                    <UnitGallary/>
+                </div>
+            </div>
+         </>
      );
 }
  
